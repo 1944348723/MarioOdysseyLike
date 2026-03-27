ef45e7338ca1ebce5e1212043393f3c60b889fdc
zcj <1944348723@qq.com>
2026-03-27 21:33:38 +0800

    实现了将输入方向从相对于世界前向转换到了相对于摄像机水平前向

--- Assets/PLAYER TWO/Platformer Project/Scripts/Player/PlayerInputSystem.cs
+++ Assets/PLAYER TWO/Platformer Project/Scripts/Player/PlayerInputSystem.cs
@@ -6,15 +6,17 @@ public class PlayerInputSystem : MonoBehaviour
     [SerializeField] private InputActionAsset inputActions;
 
     private InputAction moveAction;
+    private Camera camera;
 
     private void Awake()
     {
-
         if (inputActions == null)
         {
             Debug.LogError("Input Actions is not assigned", this);
         }
         moveAction = inputActions?["Movement"];
+
+        camera = Camera.main;
     }
 
     private void OnEnable()
@@ -25,6 +27,26 @@ public class PlayerInputSystem : MonoBehaviour
     private  void OnDisable()
     {
         inputActions?.Disable();
+    }
+
+    /// <summary>
+    /// 第三人称中，玩家的移动意图通常以“相机水平前方”为参考。
+    /// 输入系统给的是平面输入向量（x/z），本身不包含相机朝向信息，是在世界默认前方为前向的坐标系下的
+    /// 因此要用相机的 yaw（绕世界 up 轴）去旋转输入向量，得到世界空间下的移动方向。
+    /// Quaternion.AngleAxis 用“绕指定轴旋转指定角度”的方式构造这个旋转。
+    /// </summary>
+    public Vector3 GetMoveDirectionBasedOnCamera()
+    {
+        Vector3 inputDirection = GetMovementDirection();
+        if (inputDirection.sqrMagnitude == 0) return inputDirection;
+
+        // 绕竖直向上方向(y轴)旋转camera的y轴欧拉角那么多
+        // 相当于把输入坐标系从世界默认前方转到了相机水平前方
+        var rotation = Quaternion.AngleAxis(this.camera.transform.eulerAngles.y, Vector3.up);
+        Vector3 direction = rotation * inputDirection;
+        
+        direction.Normalize();
+        return direction;
     }
 
     public Vector3 GetMovementDirection()
