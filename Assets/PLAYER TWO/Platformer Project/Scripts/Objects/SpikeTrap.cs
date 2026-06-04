245ef71bb58837963b132f59db4499f782fcd427
zcj <1944348723@qq.com>
2026-06-04 10:24:32 +0800

    实现了地刺陷阱

---
+++ Assets/PLAYER TWO/Platformer Project/Scripts/Objects/SpikeTrap.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class SpikeTrap : MonoBehaviour
+{
+    [SerializeField] private Button button;
+    [SerializeField] private Collider spikeCollider;
+    [SerializeField] private Vector3 deactivateOffset = new(0, -1, 0);
+    [SerializeField] private float animationDuration = 0.2f;
+
+    private Vector3 activatePosition;
+    private Vector3 deactivatePosition;
+
+    private void Awake()
+    {
+        if (!spikeCollider)
+        {
+            spikeCollider = GetComponent<Collider>();
+        }
+    }
+
+    private void Start()
+    {
+        activatePosition = transform.position;
+        deactivatePosition = transform.position + deactivateOffset;
+        button.Pressed += Deactivate;
+        button.Released += Activate;
+    }
+
+    private void OnDestroy()
+    {
+        button.Pressed -= Deactivate;
+        button.Released -= Activate;
+    }
+
+    private void Activate()
+    {
+        spikeCollider.enabled = true;
+        DoTween.To<Vector3>(
+            () => transform.position,
+            position => transform.position = position,
+            activatePosition,
+            animationDuration
+        );
+    }
+
+    private void Deactivate()
+    {
+        spikeCollider.enabled = false;
+        DoTween.To<Vector3>(
+            () => transform.position,
+            position => transform.position = position,
+            deactivatePosition,
+            animationDuration
+        );
+    }
+}
