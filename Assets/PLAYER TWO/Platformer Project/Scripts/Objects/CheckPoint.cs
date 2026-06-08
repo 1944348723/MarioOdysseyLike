f48afc0132c62d49f53f605dfbc46b776b52e86e
zcj <1944348723@qq.com>
2026-06-08 16:03:36 +0800

    实现了检查点

---
+++ Assets/PLAYER TWO/Platformer Project/Scripts/Objects/CheckPoint.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CheckPoint : MonoBehaviour
+{
+    [SerializeField] private Transform respawn;
+    [SerializeField] private AudioClip clip;
+
+    public UnityEvent Activated;
+    private AudioSource audioSource;
+    private bool isActive = false;
+
+    private void Awake()
+    {
+        if (!TryGetComponent<AudioSource>(out audioSource))
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isActive) return;
+        if (!other.GetComponent<Player>()) return;
+        
+        isActive = true;
+        if (clip)
+        {
+            audioSource.PlayOneShot(clip);
+        }
+
+        Activated?.Invoke();
+    }
+}
